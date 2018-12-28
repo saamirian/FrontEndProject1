41536355ae59f9773b0b27b360718f5d9813f9a3
saharamirian <45035127+saharamirian@users.noreply.github.com>
2018-12-28 13:06:57 +1100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+
+const path = require('path');
+ 
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+        
+    },
+    module: {
+        rules: [{
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, {
+          test: /\.css$/,
+          use:[
+            'style-loader',
+            'css-loader'
+          ]
+        },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }
+      ],
+        
+        
+      },
+      
+      devtool: 'cheap-module-eval-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+      }
+      
+      
+};
